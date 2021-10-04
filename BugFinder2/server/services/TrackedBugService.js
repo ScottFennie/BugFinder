a1e302e561b595e9b21aa35fd88232d141396acd
Scott Fennie <s.fennie7@gmail.com>
2021-10-04 13:24:37 -0600

    server almost done

--- BugFinder2/server/services/TrackedBugService.js
+++ BugFinder2/server/services/TrackedBugService.js
@@ -1,9 +1,9 @@
 import { dbContext } from '../db/DbContext'
-import { BadRequest } from '../utils/Errors'
+import { BadRequest, Forbidden } from '../utils/Errors'
 
 class TrackedBugService {
-  async getBugById(bugId) {
-    const bug = await dbContext.Bugs.findById(bugId)
+  async getBugById(id) {
+    const bug = await dbContext.TrackedBug.findById(id).populate('bug')
     if (!bug) {
       throw new BadRequest('invalid bugId')
     }
@@ -11,11 +11,32 @@ class TrackedBugService {
   }
 
   async createTrackedBug(bugData) {
-    await this.getBugById(bugData.bugId)
     const bug = await dbContext.TrackedBug.create(bugData)
+    await this.getBugById(bug.id)
     await bug.populate('tracker')
     await bug.populate('bug')
     return bug
+  }
+
+  async deleteTrackedBug(bugId, userId) {
+    const bug = await this.getBugById(bugId)
+    if (userId !== bug.creatorId.toString()) {
+      throw new Forbidden('you did not create this bug')
+    }
+    await bug.remove()
+    return bug
+  }
+
+  async getMyTrackedBugs(accountId) {
+    const bug = await dbContext.TrackedBug.find({ creatorId: accountId }).populate('bug')
+    return bug
+  }
+
+  async getAllBugTrackers(bugId) {
+    const bug = await dbContext.Bugs.findById(bugId)
+    await bug.populate('bug')
+    await bug.populate('tracker')
+    const profs = await dbContext.Profiles.find()
   }
 }
 
